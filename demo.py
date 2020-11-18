4b8d86e5c22bfe47ef5d62d74420c92dba936a5f
zhoun59 <zhoun59@gmail.com>
2020-11-19 00:08:45 +0800

    add imagenet classfier example.

---
+++ demo.py
@@ -0,0 +1,33 @@
+import os
+import torch
+import cv2
+import numpy as np
+import torchvision.transforms as tfs
+import matplotlib.pyplot as plt
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+from models import EfficientNet
+
+
+mean = (0.485, 0.456, 0.406)
+std = (0.229, 0.224, 0.225)
+model = EfficientNet.from_pretrained('efficientnet-b0',False,True).state_dict()
+parameters = torch.load('weights/adv-efficientnet-b0.pth')
+parameters = [k for _,k in parameters.items()]
+for i,(k,v) in enumerate(model.items()):
+    model[k] = parameters[i]
+    
+torch.save(model,'weights/adv-efficientnet-b0.pth')
+
+'''
+data_dir = 'data'
+imgs_path = [os.path.join(data_dir, f) for f in os.listdir(data_dir)]
+
+imgs = [cv2.resize((cv2.imread(i)[...,::-1]/255 - mean)/std,(608,608)) for i in imgs_path]
+imgs = torch.stack([torch.from_numpy(i.astype(np.float32)) for i in imgs], 0).permute(0, 3, 1, 2)
+
+imgs = imgs[:4].cuda()
+features = model.extract_features(imgs)
+print(features.size())
+'''
