d0b496dd6e0d01ac165bf6155b0779745500c75e
zhoun59 <1842956634@qq.com>
2020-12-23 15:51:49 +0800

    update voc2coco.py

--- datasets/voc2coco.py
+++ datasets/voc2coco.py
@@ -1,144 +1,97 @@
-#!/usr/bin/python
-
-# pip install lxml
-
-import sys
+# VOC to COCO format
 import os
 import json
+import argparse
 import xml.etree.ElementTree as ET
+from tqdm import tqdm
 
+def get_args():
+    parser = argparse.ArgumentParser(
+        description = "Convert VOC to COCO format."
+    )
+    parser.add_argument('-d', '--data-path',type = str,
+        default = './', help = 'path to dataset')
+    parser.add_argument('-s', '--set_name', type = str,
+        default = 'val', help = 'set name of dataset')
+    return parser.parse_args()
 
-START_BOUNDING_BOX_ID = 1
-PRE_DEFINE_CATEGORIES = {
-    'n02691156': 0,
-    'n02419796': 1,
-    'n02131653': 2,
-    'n02834778': 3,
-    'n01503061': 4,
-    'n02924116': 5,
-    'n02958343': 6,
-    'n02402425': 7,
-    'n02084071': 8,
-    'n02121808': 9,
-    'n02503517': 10,
-    'n02118333': 11,
-    'n02510455': 12,
-    'n02342885': 13,
-    'n02374451': 14,
-    'n02129165': 15,
-    'n01674464': 16,
-    'n02484322': 17,
-    'n03790512': 18,
-    'n02324045': 19,
-    'n02509815': 20,
-    'n02411705': 21,
-    'n01726692': 22,
-    'n02355227': 23,
-    'n02129604': 24,
-    'n04468005': 25,
-    'n01662784': 26,
-    'n04530566': 27,
-    'n02062744': 28,
-    'n02391049': 29
-}
-# If necessary, pre-define category and its id
-#  PRE_DEFINE_CATEGORIES = {"aeroplane": 1, "bicycle": 2, "bird": 3, "boat": 4,
-                         #  "bottle":5, "bus": 6, "car": 7, "cat": 8, "chair": 9,
-                         #  "cow": 10, "diningtable": 11, "dog": 12, "horse": 13,
-                         #  "motorbike": 14, "person": 15, "pottedplant": 16,
-                         #  "sheep": 17, "sofa": 18, "train": 19, "tvmonitor": 20}
-
-
-def get(root, name):
-    vars = root.findall(name)
-    return vars
-
-
-def get_and_check(root, name, length):
+def get(root, name, length):
     vars = root.findall(name)
     if len(vars) == 0:
-        raise NotImplementedError('Can not find %s in %s.'%(name, root.tag))
+        raise ValueError("Can not find %s in %s." % (name, root.tag))
     if length > 0 and len(vars) != length:
-        raise NotImplementedError('The size of %s is supposed to be %d, but is %d.'%(name, length, len(vars)))
+        raise ValueError(
+            "The size of %s is supposed to be %d, but is %d."
+            % (name, length, len(vars))
+        )
     if length == 1:
         vars = vars[0]
     return vars
 
-
-def get_filename_as_int(filename):
-    try:
-        filename = os.path.splitext(filename)[0]
-        return int(filename)
-    except:
-        raise NotImplementedError('Filename %s is supposed to be an integer.'%(filename))
-
-
-def convert(xml_list, xml_dir, json_file):
-    list_fp = open(xml_list, 'r')
-    json_dict = {"images":[], "type": "instances", "annotations": [],
-                 "categories": []}
-    categories = PRE_DEFINE_CATEGORIES
-    bnd_id = START_BOUNDING_BOX_ID
-    for line in list_fp:
-        line = line.strip()
-        print("Processing %s"%(line))
-        xml_f = os.path.join(xml_dir, line)
-        tree = ET.parse(xml_f)
+def convert(pdir, set_name):
+    set_file = os.path.join(pdir, 'ImageSets', set_name + '.txt')
+    xml_set = open(set_file, 'r').readlines()
+    xml_files = [os.path.join(pdir, 'Annotations', xml[:-1] + '.xml') for xml in xml_set]
+    bnd_id = 1
+    categories = {}
+    json_dict = {"images": [], "type": "instances", "annotations": [], "categories": []}
+    for i, xml_file in tqdm(enumerate(xml_files)):
+        tree = ET.parse(xml_file)
         root = tree.getroot()
-        path = get(root, 'path')
+        path = root.findall('path')
         if len(path) == 1:
             filename = os.path.basename(path[0].text)
         elif len(path) == 0:
-            filename = get_and_check(root, 'filename', 1).text
+            filename = get(root, "filename", 1).text
         else:
-            raise NotImplementedError('%d paths found in %s'%(len(path), line))
-        ## The filename must be a number
-        image_id = get_filename_as_int(filename)
-        size = get_and_check(root, 'size', 1)
-        width = int(get_and_check(size, 'width', 1).text)
-        height = int(get_and_check(size, 'height', 1).text)
-        image = {'file_name': filename+'.JPEG', 'height': height, 'width': width,
-                 'id':image_id}
-        json_dict['images'].append(image)
-        ## Cruuently we do not support segmentation
-        #  segmented = get_and_check(root, 'segmented', 1).text
-        #  assert segmented == '0'
-        for obj in get(root, 'object'):
-            category = get_and_check(obj, 'name', 1).text
+            raise ValueError("%d paths found in %s" % (len(path), xml_file))
+        size = get(root, 'size', 1)
+        width = get(size, 'width', 1).text
+        height = get(size, 'height', 1).text
+        image = {
+            "file_name": filename,
+            "height": height,
+            "width": width,
+            "id": i,
+        }
+        json_dict["images"].append(image)
+        for obj in root.findall('object'):
+            category = get(obj, "name", 1).text
             if category not in categories:
-                new_id = len(categories)
-                categories[category] = new_id
+                categories[category] = len(categories)
             category_id = categories[category]
-            bndbox = get_and_check(obj, 'bndbox', 1)
-            xmin = int(get_and_check(bndbox, 'xmin', 1).text)
-            ymin = int(get_and_check(bndbox, 'ymin', 1).text)
-            xmax = int(get_and_check(bndbox, 'xmax', 1).text)
-            ymax = int(get_and_check(bndbox, 'ymax', 1).text)
-            assert(xmax > xmin)
-            assert(ymax > ymin)
-            o_width = abs(xmax - xmin + 1)
-            o_height = abs(ymax - ymin + 1)
-            ann = {'area': o_width*o_height, 'iscrowd': 0, 'image_id':
-                   image_id, 'bbox':[xmin, ymin, o_width, o_height],
-                   'category_id': category_id, 'id': bnd_id, 'ignore': 0,
-                   'segmentation': []}
-            json_dict['annotations'].append(ann)
-            bnd_id = bnd_id + 1
-
+            bbox = get(obj, "bndbox", 1)
+            xmin = int(get(bbox, "xmin", 1).text)
+            ymin = int(get(bbox, "ymin", 1).text)
+            xmax = int(get(bbox, "xmax", 1).text)
+            ymax = int(get(bbox, "ymax", 1).text)
+            ow = xmax - xmin
+            oh = ymax - ymin
+            assert xmax > xmin and ymax > ymin
+            ann = {
+                "area": ow * oh,
+                "iscrowd": 0,
+                "image_id": i,
+                "bbox": [xmin, ymin, ow, oh],
+                "category_id": category_id,
+                "id": bnd_id,
+                "ignore": 0,
+                "segmentation": [],
+            }
+            json_dict["annotations"].append(ann)
+            bnd_id += 1
+    
     for cate, cid in categories.items():
-        cat = {'supercategory': 'none', 'id': cid, 'name': cate}
-        json_dict['categories'].append(cat)
-    json_fp = open(json_file, 'w')
-    json_str = json.dumps(json_dict)
-    json_fp.write(json_str)
-    json_fp.close()
-    list_fp.close()
+        cat = {"supercategory": "none", "id": cid, "name": cate}
+        json_dict["categories"].append(cat)
 
+    json_file = os.path.join(pdir, 'Annotations', f'instances_{set_name}.json')
+    
+    json_fp = open(json_file, 'w')
+    json.dump(json_dict, json_fp, indent = 1)
+    json_fp.close()
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print('3 auguments are need.')
-        print('Usage: %s XML_LIST.txt XML_DIR OUTPU_JSON.json'%(sys.argv[0]))
-        exit(1)
-
-    convert(sys.argv[1], sys.argv[2], sys.argv[3])
+    args = get_args()
+    convert(args.data_path, args.set_name)
+    
